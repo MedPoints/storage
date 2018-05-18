4f2b5f6945c053e648be37125d5049f6baa4ad66
artemshulgin <artemshulgin@tutanota.com>
2018-05-18 15:01:55 +0300

    big refactoring. adding new classes and entities

---
+++ MedPoints/Storage.Core/Witness.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using Storage.Utils;
+
+namespace Storage.Core
+{
+    public class Witness : ISerializable
+    {
+        public byte[] InvocationScript;
+        public byte[] VerificationScript;
+
+        private UInt160 _scriptHash;
+        public virtual UInt160 ScriptHash
+        {
+            get
+            {
+                if (_scriptHash == null)
+                {
+                    _scriptHash = VerificationScript.ToScriptHash();
+                }
+                return _scriptHash;
+            }
+        }
+
+        public int Size => InvocationScript.GetVarSize() + VerificationScript.GetVarSize();
+
+        void ISerializable.Deserialize(BinaryReader reader)
+        {
+            InvocationScript = reader.ReadVarBytes(65536);
+            VerificationScript = reader.ReadVarBytes(65536);
+        }
+
+        void ISerializable.Serialize(BinaryWriter writer)
+        {
+            writer.WriteVarBytes(InvocationScript);
+            writer.WriteVarBytes(VerificationScript);
+        }
+
+        public JObject ToJson()
+        {
+            JObject json = new JObject();
+            json["invocation"] = InvocationScript.ToHexString();
+            json["verification"] = VerificationScript.ToHexString();
+            return json;
+        }
+    }
+}
---
+++ MedPoints/Storage.Network/IInventory.cs
@@ -0,0 +1,13 @@
+﻿using Storage.Core;
+
+namespace Storage.Network
+{
+    public interface IInventory : IVerifiable
+    {
+        UInt256 Hash { get; }
+
+        InventoryType InventoryType { get; }
+
+        bool Verify();
+    }
+}
